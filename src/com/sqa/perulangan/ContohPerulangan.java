1822d810f32baeb595903e8b10cefce411dec035
Ferdin Permana Putra <Ferdin Permana Putra@DESKTOP-NUPDC45>
2021-11-24 11:12:07 +0700

    FerdinPermana

---
+++ src/com/sqa/perulangan/ContohPerulangan.java
@@ -0,0 +1,11 @@
+package com.sqa.perulangan;
+
+public class ContohPerulangan {
+	public static void main(String[] args) {
+		//Aplikasi Raraban
+		int n=8;
+		for(int i=1; i<=20; i++) {
+			System.out.printf("%d x %d = %d%n", n, i, n*i);
+		}
+	}
+}
